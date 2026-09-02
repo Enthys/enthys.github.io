10b9105162978e960e0e8b1a7585881cf0e233f3
Asen Mihaylov <asenj.mihaylov@gmail.com>
2026-09-02 19:59:20 +0300

    update

--- format.js
+++ format.js
@@ -1,31 +1,31 @@
-const fs = require("fs")
+// const fs = require("fs")
 
-const m = new Map();
+// const m = new Map();
 
-m.set(`#skills .item {
-  width: 16em;
-}`,
-    `#skills .item {
-    width: 15em;
-    text-align: center;
-    border-right: 1px solid #ddd;
-    padding: 0;
-}
+// m.set(`#skills .item {
+//   width: 25em;
+// }`,
+//     `#skills .item {
+//     width: 15em;
+//     text-align: center;
+//     border-right: 1px solid #ddd;
+//     padding: 0;
+// }
 
-#skills .item:last-child {
-    border-right: none;
-}`
-)
+// #skills .item:last-child {
+//     border-right: none;
+// }`
+// )
 
-fs.readFile("index.html", 'utf8', (err, data) => {
-    if (err) throw err;
+// fs.readFile("index.html", 'utf8', (err, data) => {
+//     if (err) throw err;
 
-    m.forEach((v, k) => {
-        data = data.replace(k, v);
-    });
+//     m.forEach((v, k) => {
+//         data = data.replace(k, v);
+//     });
 
-    fs.writeFile("index.html", data, (err) => {
-        if (err) throw err;
-    });
-});
+//     fs.writeFile("index.html", data, (err) => {
+//         if (err) throw err;
+//     });
+// });
 
